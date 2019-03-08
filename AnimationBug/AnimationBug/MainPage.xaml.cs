6703089efac41807f5b6900b0bb1e15da9b4f0e1
tpauchard <t.pauchard@hokoha.com>
2019-03-08 12:01:45 +0100

    init

---
+++ AnimationBug/AnimationBug/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AnimationBug
+{
+    public partial class MainPage : ContentPage
+    {
+
+        double widthScreen;
+        bool loop = true;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GetScreenMetrics();
+            AnimateBoxView();
+        }
+
+        /// <summary>
+        /// Get Screen metrics
+        /// </summary>
+        private void GetScreenMetrics()
+        {
+            var metrics = DeviceDisplay.MainDisplayInfo;
+            widthScreen = metrics.Width / metrics.Density;
+        }
+
+        /// <summary>
+        /// Animate boxview
+        /// </summary>
+        private async void AnimateBoxView()
+        {
+            await Task.Run(async () =>
+            {
+                while (loop)
+                {
+                    try
+                    {
+                        await bvAnim.TranslateTo(-widthScreen, bvAnim.TranslationY, 2000);
+                        bvAnim.TranslationX = widthScreen;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            });
+        }
+    }
+}
